b866cf9a9e232160df19be7053fba994b89c96b6
mike <mike>
1995-12-16 20:28:38 +0000

    Special files- related changes

--- semfile.h
+++ semfile.h
@@ -7,7 +7,10 @@
 #define __SEMFILE_H
 
 #define PRINTER_COMMAND		"lpr -h"
-#define PRINTER_SPOOL_FILE	"/tmp/printout.cobol.tmp"
+#define PRINTER_SPOOL_FILE	"/tmp/cobol.prn"
+
+#define PRINTER_STREAM_NAME	"printer"
+#define DISPLAY_STREAM_NAME	"stdout"
 
 /*-------------------------------------------*/
   void 	FileDecl (void);
@@ -23,6 +26,9 @@
   void	SetOrganization (char * org);
   void	SetRelativeKey (void);
   void	SetRecordKey (void);
+  void	OpenSpecialFiles (void);
+  void	CloseSpecialFiles (void);
+  void	FlushPrinterOutput (void);
 /*-------------------------------------------*/
 
 #endif
