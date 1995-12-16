0515e362c2153bef46788c25026b33fc40b0cd72
mike <mike>
1995-12-16 20:28:38 +0000

    Minor code formatting

--- cobfunc.h
+++ cobfunc.h
@@ -6,12 +6,21 @@
 #ifndef __COBFUNC_H
 #define __COBFUNC_H
 
-/*-------------------------------------------------------------*/
-  int   _alphabetic (char * str);
-  int   _alphab_up_low (char * str, int what);
+/*-------------------------------------------------------------------*/
+  int 	_alphabetic (char * str);
+  int 	_alphab_up_low(char * str, int what);
   void 	_RuntimeError (char * message);
-  void 	_AssignVar (char * var1, char * var2, int p1, int p2);
-/*-------------------------------------------------------------*/
+  void 	_ReadStringVar (FILE * stream, char * var, char * pic);
+  void 	_ReadIntegerVar (FILE * stream, long int var, char * pic);
+  void 	_ReadFloatVar (FILE * stream, double var, char * pic);
+  void 	_WriteStringVar (FILE * stream, char * var, char * pic);
+  void 	_WriteIntegerVar (FILE * stream, long int var, char * pic);
+  void 	_IntegerToString (char * string, long int var, char * pic);
+  void 	_WriteFloatVar (FILE * stream, double var, char * pic);
+  void 	_FloatToString (char * string, double var, char * pic);
+  void 	_AssignVarString (char * var1, char * var2, int p1, int p2);
+  long int 	_RoundResult (double num);
+/*-------------------------------------------------------------------*/
 
 #endif
 
