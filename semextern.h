6cda60b13c9269a9625d68e396835ece7063b510
mike <mike>
1995-12-16 20:28:38 +0000

    Global configuration structure is here now

--- semextern.h
+++ semextern.h
@@ -15,11 +15,25 @@
 #include "semsedef.h"
 #include "semutil.h"
 
+#define MAX_FILENAME	30
+
+/***--| Cobcy global configuration options |--***/
+typedef struct {
+    /* Filename locations */
+    char 	SourceFile [MAX_FILENAME];	/* Input COBOL file */
+    char 	CodeFile [MAX_FILENAME];	/* Output .c file */
+    char 	DeclFile [MAX_FILENAME];	/* Output .h file */
+
+    /* Build options */
+    BOOL	GenDebug;	/* Generate debugging code? */
+} CobcyConfigType;
+
 // All of these are defined in seminit.cc
 extern Stack<StackEntry> SemStack;
 extern HashTable<CobolSymbol> SymTable;
 extern int NestingLevel;
 extern ofstream codef, declf;
+extern CobcyConfigType CobcyConfig;
 
 #endif
 
