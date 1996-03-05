f229b9e7643670a8ec37751155cc246fda3d34c3
mike <mike>
1996-03-05 21:26:28 +0000

    Fixed the .c.h bug/feature Indentation Shielded debug messages with NDEBUG

--- main.cc
+++ main.cc
@@ -34,23 +34,23 @@ int main (int argc, char ** argv)
 {
     SetInitialConfiguration();
     if (ProcessFlags (argc, argv) == S_ERROR) {
-       Usage (argv[0]);
-       return (2);
+	Usage (argv[0]);
+	return (2);
     }
     
     if ((yyin = fopen (CobcyConfig.SourceFile, "r")) == NULL) {
-       cerr << "FATAL: Could not open '" << CobcyConfig.SourceFile << "'!\n";
-       return (1);
+	cerr << "FATAL: Could not open '" << CobcyConfig.SourceFile << "'!\n";
+	return (1);
     }
     else
-       cout << "Compiling " << CobcyConfig.SourceFile << " ...\n";
+	cout << "Compiling " << CobcyConfig.SourceFile << " ...\n";
     
     yyparse();
     fclose (yyin);
 
     if (ErrorOccured()) {
-       unlink (CobcyConfig.CodeFile);
-       unlink (CobcyConfig.DeclFile);
+	unlink (CobcyConfig.CodeFile);
+	unlink (CobcyConfig.DeclFile);
     }
 
     return (0);
@@ -59,7 +59,7 @@ int main (int argc, char ** argv)
 void Usage (char * progname)
 {
     cout << "\n";
-    cout << "Cobol to C compiler v0.1, Copyright (c) Mike Sharov, 1995\n";
+    cout << "Cobol to C compiler v0.1, Copyright (c) Mike Sharov, 1996\n";
     cout << "Usage:\n";
     cout << "\t" << progname << " [options] <file.cob>\n";
     cout << "\n";
@@ -74,7 +74,7 @@ void Usage (char * progname)
 
 int ProcessFlags (int argc, char ** argv)
 {
-int i;
+int i, NameLength;
 enum {
     FlagMode,
     SourceFileMode,	/* This will be the default */
@@ -84,54 +84,63 @@ BOOL SourceSet = FALSE, OutputSet = FALSE;
 
     mode = SourceFileMode;
     for (i = 1; i < argc; ++ i) {
-       if (argv[i][0] == '-')
-	  mode = FlagMode;
+	if (argv[i][0] == '-')
+	    mode = FlagMode;
 
-       switch (mode) {
-	  case FlagMode:
-	  	if (strcmp (argv[i], "-o") == 0)
-		   mode = OutputFileMode;
-	  	else if (strcmp (argv[i], "-g") == 0)
-		   CobcyConfig.GenDebug = TRUE;
+	switch (mode) {
+	    case FlagMode:
+		if (strcmp (argv[i], "-o") == 0)
+		    mode = OutputFileMode;
+		else if (strcmp (argv[i], "-g") == 0)
+		    CobcyConfig.GenDebug = TRUE;
 
 		// If not -o, revert to looking for the source file
 		if (mode != OutputFileMode)
-		   mode = SourceFileMode;
+		    mode = SourceFileMode;
 		break;
-	  case SourceFileMode:
-	 	if (!SourceSet) {
-		   strcpy (CobcyConfig.SourceFile, argv[i]);
-		   if (!OutputSet) {
-		      strcpy (CobcyConfig.CodeFile, CobcyConfig.SourceFile);
-		      strcat (CobcyConfig.CodeFile, ".c");
-		      strcpy (CobcyConfig.DeclFile, CobcyConfig.SourceFile);
-		      strcat (CobcyConfig.DeclFile, ".h");
-		   }
-		   SourceSet = TRUE;
+	    case SourceFileMode:
+		if (!SourceSet) {
+		    strcpy (CobcyConfig.SourceFile, argv[i]);
+		    if (!OutputSet) {
+			strcpy (CobcyConfig.CodeFile, CobcyConfig.SourceFile);
+			strcat (CobcyConfig.CodeFile, ".c");
+			strcpy (CobcyConfig.DeclFile, CobcyConfig.SourceFile);
+			strcat (CobcyConfig.DeclFile, ".h");
+		    }
+		    SourceSet = TRUE;
 		}
-	  	break;
-	  case OutputFileMode:
-	        if (!OutputSet) {
-		   strcpy (CobcyConfig.CodeFile, argv[i]);
-		   strcpy (CobcyConfig.DeclFile, argv[i]);
-		   strcat (CobcyConfig.DeclFile, ".h");
-		   mode = SourceFileMode;
-		   OutputSet = TRUE;
+		break;
+	    case OutputFileMode:
+		if (!OutputSet) {
+		    strcpy (CobcyConfig.CodeFile, argv[i]);
+		    strcpy (CobcyConfig.DeclFile, argv[i]);
+		    /* If possible, remove the ending .c extension 	*/
+		    /* Do this by replacing the dot with EOS 		*/
+		    NameLength = strlen (CobcyConfig.DeclFile);
+		    if (CobcyConfig.DeclFile [NameLength - 1] == 'c' &&
+		        CobcyConfig.DeclFile [NameLength - 2] == '.')
+		        CobcyConfig.DeclFile [NameLength - 2] = '\x0';
+
+		    strcat (CobcyConfig.DeclFile, ".h");
+		    mode = SourceFileMode;
+		    OutputSet = TRUE;
 		}
-	  	break;
-       }
+		break;
+	}
     }
 
+#ifndef NDEBUG
     cerr << "Reading from " << CobcyConfig.SourceFile << "\n";
     cerr << "Compiling into " << CobcyConfig.CodeFile << " and ";
     cerr << CobcyConfig.DeclFile << "\n";
+#endif
 
     if (!SourceSet) {
-       cerr << "No cobol file specified\n";
-       return (S_ERROR);
+	cerr << "No cobol file specified\n";
+	return (S_ERROR);
     }
     else
-       return (S_OK);
+	return (S_OK);
 }
 
 void SetInitialConfiguration (void)
